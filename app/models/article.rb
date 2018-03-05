e060725d9c2859042b830120362ef508bc3274c6
tunagohan <kazuxtuna@gmail.com>
2018-03-05 21:40:57 +0900

    Aritcle用のmodelを作成

---
+++ app/models/article.rb
@@ -0,0 +1,13 @@
+class Article < ApplicationRecord
+  def self.find(input)
+    if input.is_a?(Integer)
+      super
+    else
+      find_by_title(input)
+    end
+  end
+
+  def to_param
+    title
+  end
+end
