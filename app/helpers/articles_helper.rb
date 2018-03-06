dd78dd0effbc10cb5257d498481af348d4f85b4d
tunagohan <kazuxtuna@gmail.com>
2018-03-07 00:01:49 +0900

    マークダウン反映

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,48 @@
 module ArticlesHelper
+  require "redcarpet"
+  require "coderay"
+
+  class HTMLwithCoderay < Redcarpet::Render::HTML
+    def block_code(code, language)
+      language = language.split(':')[0]
+
+      case language.to_s
+      when 'rb'
+        lang = 'ruby'
+      when 'yml'
+        lang = 'yaml'
+      when 'css'
+        lang = 'css'
+      when 'html'
+        lang = 'html'
+      when ''
+        lang = 'md'
+      else
+        lang = language
+      end
+
+      CodeRay.scan(code, lang).div
+    end
+  end
+
+  def markdown(text)
+    html_render = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
+
+    options = {
+      :autolink => true,
+      :space_after_headers => true,
+      :no_intra_emphasis => true,
+      :tables => true,
+      :space_after_headers => true,
+      :fenced_code_blocks => true,
+      :hard_wrap => true,
+      :strikethrough => true,
+      :filter_html => true
+    }
+    unless @markdown
+      @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true), options)
+    end
+
+    @markdown.render(text).html_safe
+  end
 end
