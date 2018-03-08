e65bff88415680852726ca5caa40cd604e1318bf
tunagohan <kazuxtuna@gmail.com>
2018-03-09 00:10:43 +0900

    bootstrapの適用といらないscssを削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,5 @@
+//= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
