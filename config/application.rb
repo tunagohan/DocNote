4bdb25b3f0d1814f62682c0b4d8fcad8ecb6ed97
tunagohan <kazuxtuna@gmail.com>
2018-03-09 01:43:34 +0900

    複数の修正 ・indexにbootstrapを適用 ・日付の修正

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module App
     config.load_defaults 5.1
     config.i18n.default_locale = :ja
     config.action_controller.include_all_helpers = false
+    config.time_zone = 'Asia/Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
