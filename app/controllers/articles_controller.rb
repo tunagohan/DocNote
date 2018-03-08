8b3fd08af6f1c4f1be4072caccf203c4e7a97d04
tunagohan <kazuxtuna@gmail.com>
2018-03-08 18:13:33 +0900

    :bug:サインインしている状態でのみ投稿 ・バグありコミット

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < InheritedResources::Base
-
+  before_action :authenticate_user!
   private
 
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+  def article_params
+    params.require(:article).permit(:title, :body, :user_id)
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def set_article
+    @articles = Article.find(params[:id])
+  end
 end
 
