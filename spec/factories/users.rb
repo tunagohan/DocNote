06914408d977f0d64eb8988cb3f2926c5c700693
tunagohan <kazuxtuna@gmail.com>
2018-03-13 10:45:18 +0900

    仮のspecファイルの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    username 'fizzbazz'
+    email 'foo@example.com'
+    password 'hogehoge'
+    password_confirmation 'hogehoge'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it 'is valid with a mail and account' do
-    user = User.new(
-      username: 'fizzbazz',
-      email: 'foo@example.com',
-      password: 'hogehoge',
-      password_confirmation: 'hogehoge'
-    )
+  it 'is valid with a username, email, password and password_confirmation' do
+    user = FactoryBot.build(:user)
     expect(user).to be_valid
   end
 
   it 'is invalid without a password' do
-    user = User.new(password: nil)
+    user = FactoryBot.build(:user, password: nil)
     user.valid?
     expect(user.errors[:password]).to include("can't be blank")
   end
