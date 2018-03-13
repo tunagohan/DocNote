a7b9eef7b06951ae112ba231ac39ac80df36c0f4
tunagohan <kazuxtuna@gmail.com>
2018-03-13 10:15:41 +0900

    userのspecを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is valid with a mail and account' do
+    user = User.new(
+      username: 'fizzbazz',
+      email: 'foo@example.com',
+      password: 'hogehoge',
+      password_confirmation: 'hogehoge'
+    )
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a password' do
+    user = User.new(password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
 end
