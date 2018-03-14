3a4a8bf785b4ed8213909143f263e9df422acdd4
tunagohan <kazuxtuna@gmail.com>
2018-03-15 01:31:18 +0900

    インデント修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,32 +8,32 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound,   with: :handle_404 unless Rails.env.development?
 
   def configure_permitted_parameters
-     added_attrs = [:username, :email, :password, :password_confirmation]
-     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    added_attrs = [:username, :email, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
    end
 
-   def handle_500(exception = nil)
-     logger.info "Rendering 500 with exception: #{exception.message}" if exception
+  def handle_500(exception = nil)
+    logger.info "Rendering 500 with exception: #{exception.message}" if exception
+    if request.xhr?
+      # Ajaxのための処理
+      render json: { error: '500 error' }, status: 500
+    else
+      render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+    end
+  end
 
-     if request.xhr?
-       # Ajaxのための処理
-       render json: { error: '500 error' }, status: 500
-     else
-       render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
-     end
-   end
+  def handle_404(exception = nil)
+    logger.info "Rendering 404 with exception: #{exception.message}" if exception
+    if request.xhr?
+      # Ajaxのための処理
+      render json: { error: '404 error' }, status: 404
+    else
+      render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+    end
+  end
 
-   def handle_404(exception = nil)
-     logger.info "Rendering 404 with exception: #{exception.message}" if exception
 
-     if request.xhr?
-       # Ajaxのための処理
-       render json: { error: '404 error' }, status: 404
-     else
-       render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
-     end
-   end
 
 end
