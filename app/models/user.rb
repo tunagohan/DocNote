c5ae1d67d1a5a9f03b919dae49b9dff359889c18
tunagohan <kazuxtuna@gmail.com>
2018-03-21 00:55:49 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :articles
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :timeoutable
+
+  validates :username,
+            uniqueness: { case_sensitive: :false },
+            length: { minimum: 4, maximum: 20 }
 end
