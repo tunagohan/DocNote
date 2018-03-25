428ed6f37af5a87834204951e71f5e52d9ffb4d7
tunagohan <kazuxtuna@gmail.com>
2018-03-25 15:13:13 +0900

    awesome helperの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 require 'navigation_helper.rb'
 module ApplicationHelper
   include NavigationHelper
+  include FontAwesome::Rails::IconHelper
   def simple_time(time)
     time.strftime("%Y年%m月%d日 %H:%M")
   end
